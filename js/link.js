8d90010498f028946f45df6cc28f06e105708e6e
SultonbekovSarvarbek <45137425+SultonbekovSarvarbek@users.noreply.github.com>
2019-05-20 14:40:09 +0500

    Update link.js

--- js/link.js
+++ js/link.js
@@ -6,22 +6,52 @@ $(function()
         introH=$("#intro").innerHeight(),
         scrollOffset= $(window).scrollTop();
 
+        checkscroll(scrollOffset);
+
     $(window).on("scroll",function(){
         scrollOffset=$(this).scrollTop();
         
+        checkscroll(scrollOffset);
     });
+        function checkscroll(scrollOffset)
+        {
+            scrollOffset=$(this).scrollTop();
+        if(scrollOffset >= introH)
+        {
+            header.addClass("header--fixed");
+           
+        }
+        else 
+        {
+            header.removeClass("header--fixed");
+        }
+
+        }
 
     $("[data-scroll]").on("click",function(event)
     {
         event.preventDefault();
 
-        var blockId= $(this).data('scroll'),
+        var $this=$(this),
+            blockId= $(this).data('scroll'),
             blockOffset=$(blockId).offset().top;
+
+            $("#nav a").removeClass("active");
+            $this.addClass("active");
 
         $("html, body").animate({
             scrollTop: blockOffset
         },1000);
 
+
+    });
+
+
+    $("#nav_toggle").on("click",function(event)
+    {
+        event.preventDefault();
+
+        $("#nav").toggleClass("active");
 
     });
 
